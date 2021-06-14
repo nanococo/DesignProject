a884ee993f53b7398b2406f7db8ec0a8164eeda8
Sebastian <salpizar@outcoding.com>
2021-06-14 15:36:26 -0600

    Added first steps

--- Reques/App_Start/BundleConfig.cs
+++ Reques/App_Start/BundleConfig.cs
@@ -22,9 +22,13 @@ namespace Reques
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                    "~/Content/style.css"
+                ));
         }
     }
 }
